cef1e9aba5bc53a8c1f7f8cacb549a522e13eb2d
Alexander Montealegre Ramirez <amontealegrer@gmail.com>
2021-12-16 19:14:54 -0500

    Change in App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>Deployment React from Github Actions to S3 AWS</h1>
         </p>
         <a
           className="App-link"
